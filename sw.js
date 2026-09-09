510acd81e9579ec39c7b814c03c3c22dc034f5d7
danieledanvieira19-source <daniele.danvieira19@gmail.com>
2026-09-09 13:31:23 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "villa-kids-v57-offline";
+const CACHE_NAME = "villa-kids-v61-offline";
 const APP_SHELL = [
   "./", "./index.html", "./app.html",
   "./configurar-equipe.html", "./convidados-festa.html", "./documentos-festa.html",
